b7f631052869a41014c02b506fd7ac15a6fb1500
alibahcecioglu <alibahcecioglu9@gmail.com>
2022-05-23 23:53:13 +0300

    initial

---
+++ src/main/java/com/sha/springbootmicroservicetransaction/service/ITransactionService.java
@@ -0,0 +1,15 @@
+package com.sha.springbootmicroservicetransaction.service;
+
+import com.sha.springbootmicroservicetransaction.model.Transaction;
+
+import java.util.List;
+
+
+public interface ITransactionService
+{
+    Transaction saveTransaction(Transaction transaction);
+
+    void deleteTransaction(Long transactionId);
+
+    List<Transaction> findAllTransactionsOfUser(Long userId);
+}
